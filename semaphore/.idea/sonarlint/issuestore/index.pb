a190ea58e8f76901bb19a7f68e2c5c524c2d5f43
ejhxlng <jiahe.liang@ericsson.com>
2020-11-30 17:25:05 +0800

    remove comments

---
+++ semaphore/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+P
+ src/main/java/SemaphoreTest.java,c\a\cac50ffe5e4057e5587d9c94bc8505a3f02cabc9
--- semaphore/src/main/java/SemaphoreTest.java
+++ semaphore/src/main/java/SemaphoreTest.java
@@ -1,20 +1,7 @@
-/*
- * ----------------------------------------------------------------------
- * COPYRIGHT Ericsson 2020
- *
- * The copyright to the computer program(s) herein is the property of
- * Ericsson Inc. The programs may be used and/or copied only with written
- * permission from Ericsson Inc. or in accordance with the terms and
- * conditions stipulated in the agreement/contract under which the
- * program(s) have been supplied.
- * ----------------------------------------------------------------------
- */
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
-/** @author ejhxlng */
 public class SemaphoreTest {
 
   public static void main(String[] args) {
