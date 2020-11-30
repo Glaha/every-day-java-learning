1f648b097fbddfc002cab164858bec3e1adce856
ejhxlng <jiahe.liang@ericsson.com>
2020-11-30 18:06:02 +0800

    hanoi tower

--- algorithm/src/main/java/HanoiTower.java
+++ algorithm/src/main/java/HanoiTower.java
@@ -1,2 +1,21 @@
 public class HanoiTower {
+    public int run(int i) {
+        return run(i, 'A', 'B', 'C');
+    }
+
+    private int run(int i, char a, char b, char c) {
+        int m = 0;
+        if (i == 1) {
+            System.out.println("1 plate " + a + "->" + c);
+            return 1;
+        } else {
+            //System.out.println("multi plates " + a + "->" + b);
+            m = m + run(i - 1, a, c, b);
+            System.out.println("1 plate " + a + "->" + c);
+            m = m + 1;
+            //System.out.println("multi plates " + b + "->" + c);
+            m = m + run(i - 1, b, a, c);
+        }
+        return m;
+    }
 }
--- algorithm/src/test/java/HanoiTowerTests.java
+++ algorithm/src/test/java/HanoiTowerTests.java
@@ -1,3 +1,20 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class HanoiTowerTests {
+
+    @Test
+    public void givenBlockNumberThenReturnTimes() {
+        HanoiTower hanoiTower = new HanoiTower();
+        int times = hanoiTower.run(3);
+        Assertions.assertEquals(7, times);
+        System.out.println("-------------------------------");
+
+        times = hanoiTower.run(2);
+        Assertions.assertEquals(3, times);
+        System.out.println("-------------------------------");
+
+        times = hanoiTower.run(4);
+        Assertions.assertEquals(15, times);
+    }
 }
